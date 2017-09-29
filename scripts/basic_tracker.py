dcb8f872d25489fa25a30b21386f58db1653a49e
Harsh Sinha <harshsin@iitk.ac.in>
2017-09-29 20:02:35 +0530

    minor bugfix for trackbar storage

--- scripts/basic_tracker.py
+++ scripts/basic_tracker.py
@@ -34,14 +34,14 @@ cv2.createTrackbar('Red High',  'Color Range', 255, 255, nothing)
 color_low  = np.array([0, 0, 0])
 color_high = np.array([250, 105, 50])
 pts = deque(maxlen=args["buffer"])
- 
+
 # if a video path was not supplied, grab the reference
 # to the webcam, do calibration only in this case
 if not args.get("video", False):
     camera = cv2.VideoCapture(0)
     if args.get("calibration", False):
         CALIB_MODE = True
- 
+
 # otherwise, grab a reference to the video file
 else:
     camera = cv2.VideoCapture(args["video"])
@@ -61,12 +61,12 @@ while True:
     # Blobs
     mask = cv2.inRange(hsv, color_low, color_high)
     mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)    
+    mask = cv2.dilate(mask, None, iterations=2)
 
     # Show trackbars and find their position
     # use them to set new threshold
     cv2.imshow("Color Range", mask)
-    bl = cv2.getTrackbarPos("Blue Low",  "Color Range")    
+    bl = cv2.getTrackbarPos("Blue Low",  "Color Range")
     gl = cv2.getTrackbarPos("Green Low", "Color Range")
     rl = cv2.getTrackbarPos("Red Low",   "Color Range")
     bh = cv2.getTrackbarPos("Blue High", "Color Range")
@@ -79,7 +79,7 @@ while True:
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)[-2]
     center = None
- 
+
 	# only proceed if at least one contour was found
     if len(cnts) > 0:
         # find the largest contour in the mask, then use
@@ -98,7 +98,7 @@ while True:
                         (int(x - radius), int(y - radius)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 0, 2)
             cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
- 
+
 	# update the points queue
     pts.appendleft(center)
     	# loop over the set of tracked points
@@ -107,11 +107,11 @@ while True:
 		# them
 		if pts[i - 1] is None or pts[i] is None:
 			continue
- 
+
 		# draw the connecting lines
 		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
 		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
- 
+
 	# show the frame to our screen
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
@@ -119,12 +119,12 @@ while True:
     # if the 's' key is pressed, store the trackbar values.
     if key == ord('s'):
         f = open(datetime.datetime.now().strftime ("%Y%m%d%H%M"), 'w')
-        f.write(bl + ' ' + gl + ' ' + rl + ' ' + bh + ' ' + gh + ' ' + rh)
+        f.write(str(bl) + ' ' + str(gl) + ' ' + str(rl) + ' ' + str(bh) + ' ' + str(gh) + ' ' + str(rh))
         f.close()
 	# if the 'q' key is pressed, stop the loop
     if key == ord("q"):
 		break
- 
+
 # cleanup the camera and close any open windows
 camera.release()
 cv2.destroyAllWindows()
