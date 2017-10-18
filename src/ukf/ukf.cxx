6db5aa867b92d829d5a8a53fac64f703a702539a
Harsh Sinha <harshsin@iitk.ac.in>
2017-10-18 17:01:45 +0530

    DEBUGGING : ukf node, currently untested

--- src/ukf/ukf.cxx
+++ src/ukf/ukf.cxx
@@ -2,19 +2,19 @@
 ************* Driver Program for UKF in GTLand ************
 **********************************************************/
 
-#include <update/UKF_update.h>
-#include <predict/UKF_predict.h>
+#include "update/UKF_update.h"
+#include "predict/UKF_predict.h"
 #include <geometry_msgs/Vector3.h>
-#include <ros.h>
-#include <ypr.h>
+#include <ros/ros.h>
+#include <cmath>
 
 double const dt = 1/24.5; // Frequency from the SITL
 
 double x[6] = {0};
 double z[3]; // u, v, r objects location in pixel frame 
 double P[36];
-const double RE[36];
-const double QE[9] = [2*1e-5, 0, 0, 0, 1e-5, 0, 0, 0, 1e-5];
+double RE[36];
+double QE[9] = {2*1e-5, 0, 0, 0, 1e-5, 0, 0, 0, 1e-5};
 
 // Internal matrix for the camera
 // From calib for my webcam
@@ -28,20 +28,22 @@ const double R_actual = 0.0212;
 bool xinitialized = false;
 
 ros::NodeHandle nh;
-ros::Publisher gimbal_ang, current_pose, current_velo;
+ros::Publisher gimbal_asetp, current_pose, current_velo;
 ros::Subscriber cam_sub;
 
 // single callback contains all publishers
-void cam_cb (geometry_msgs::Twist &camera)
+void cam_cb (const geometry_msgs::Vector3 &camera)
 {
-	z[1] = camera.linear.x;
-	z[2] = camera.linear.y;
-	z[0] = camera.linear.z;
+	z[1] = camera.x;
+	z[2] = camera.y;
+	z[0] = camera.z;
 
 	// Initialize x with the with the first values from camera
 	if (!xinitialized) {
 		double dist = f*R_actual / z[0];
-		double camz = dist * f / ((z[1] - cx)^2 + (z[2] - cy)^2 + f*f)^0.5;
+		double camz = dist * f / std::pow (std::pow (z[1] - cx, 2)
+										 + std::pow (z[2] - cy, 2)
+								  		 + std::pow (f, 2), 0.5);
 		x[0] = (z[1] - cx) * camz / f;
 		x[1] = (z[2] - cy) * camz / f;
 		x[2] = camz;
@@ -68,15 +70,12 @@ void cam_cb (geometry_msgs::Twist &camera)
 	current_pose.publish (position);
 	current_velo.publish (velocity);
 
-	// Publish the desired gimbal angles
-	yaw_desired = asin (x[0] / (x[0]^2 + x[2]^2)^0.5);
-	pitch_desired = asin (x[1] / (x[1]^2 + x[2]^2)^0.5);
-	roll_desired = 0;
-
-	ypr desired_angles;
-	desired_angles.yaw = yaw_desired;
-	desired_angles.pitch = pitch_desired;
-	desired_angles.roll = roll_desired;
+	// Publish the desired gimbal angle
+	// x -- yaw, y -- pitch, z -- roll
+	geometry_msgs::Vector3 desired_angles;
+	desired_angles.x = asin (x[0] / std::pow ((x[0]*x[0] + x[2]*x[2]), 0.5));
+	desired_angles.y = asin (x[1] / std::pow ((x[1]*x[1] + x[2]*x[2]), 0.5));
+	desired_angles.z = 0;
 
 	gimbal_asetp.publish (desired_angles);
 
@@ -88,11 +87,11 @@ int main (int argc, char** argv)
 
 	ros::init (argc, argv, "QUAD_UKF_node");
 
-	gimbal_asetp = nh.advertise<gtland::ypr>("/gimbal_asetp", 10);
+	gimbal_asetp = nh.advertise<geometry_msgs::Vector3>("/gimbal_asetp", 10);
 	current_pose = nh.advertise<geometry_msgs::Vector3>("/current_position", 10);
 	current_velo = nh.advertise<geometry_msgs::Vector3>("/current_velocity", 10);
 
-	cam_sub = nh.Subscribe ("/camera_pose", 100, cam_cb);
+	cam_sub = nh.subscribe ("/camera_pose", 100, cam_cb);
 
 	ros::Rate loop_rate (100);
 
@@ -103,7 +102,7 @@ int main (int argc, char** argv)
 	}
 
 	while (ros::ok()) {
-		ros::SpinOnce ();
+		ros::spinOnce ();
 		loop_rate.sleep ();
 	}
 	return 0;
