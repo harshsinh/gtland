2872ccaedfb99d8122a72e19c794dd847f2bde91
Harsh Sinha <harshsin@iitk.ac.in>
2017-11-04 00:05:48 +0530

    making progress on cpp based detection

--- src/visualdetector/src/detector2.cxx
+++ src/visualdetector/src/detector2.cxx
@@ -1,7 +1,13 @@
 #include <ros/ros.h>
 #include <iostream>
-#include "CVInclude.h"
+// #include "CVInclude.h"
 #include <geometry_msgs/Vector3.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <image_transport/image_transport.h>
 
 cv::VideoCapture cap;
 
@@ -10,11 +16,14 @@ int main (int argc, char** argv)
 	//ros node stuff
 	ros::init (argc, argv, "detector2");
 	ros::NodeHandle nh;
-	ros::Rate loop_rate (50);
-
+	ros::Rate loop_rate (500);
+	sensor_msgs::ImagePtr msg;
 	image_transport::ImageTransport it (nh);
 	image_transport::Publisher pub = it.advertise ("detected_image", 1);
 
+	// cv stuff
+	cv::Mat frame;
+	
 	//camera choice
 	int camera = argv [1][0] - 48;
 
@@ -23,17 +32,47 @@ int main (int argc, char** argv)
 
 	cap.open (camera);
 
-	cv::namedWindow ("original");
+	cv::namedWindow ("thresholded");
 	cv::startWindowThread();
+	int th;
+	cv::createTrackbar ("Threshold", "thresholded", &th, 255);
 
 	if (!cap.isOpened()) {
 		std::cout << "Unable to open camera "<< camera << std::endl;
 		ROS_ERROR_STREAM ("Unable to open camera");
 		return -1;
 	}
+	
+	while (nh.ok()) {
 
-	cv::Mat frame;
-	sensor_msgs::ImagePtr msg;
+		cap >> frame;
+		if (!frame.empty()) {
 
+			//find marker
+			cv::Mat resized, blurred, thresholded, gray, dilated;
+			cv::cvtColor (frame, gray, CV_BGR2GRAY);
+			cv::resize (gray, resized, cv::Size(), 0.50, 0.50, CV_INTER_LINEAR);
+			cv::GaussianBlur (resized, blurred,  cv::Size(11, 11), 0, 0);
+			cv::threshold (blurred, thresholded, th, 255, CV_THRESH_BINARY);
+
+			// show stuff
+			cv::imshow ("original", frame);
+			cv::imshow ("resized", resized);
+			cv::imshow ("blurred", blurred);
+			cv::imshow ("thresholded", thresholded);
+			// stop if "q" is pressed
+			if (cv::waitKey(1) == 113)
+				break;
+		}
+
+		else {
+			ROS_ERROR_STREAM ("No Image found!");
+			return -1;
+		}
+
+		ros::spinOnce();
+		loop_rate.sleep();
+
+	}
 	return 0;
 }
