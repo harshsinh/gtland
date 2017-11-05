49080feb72e474addc84875554903b26eec3c28b
Harsh Sinha <harshsin@iitk.ac.in>
2017-11-05 12:26:18 +0530

    changed detector2, now intermidiate working at 30 hz

--- src/visualdetector/src/detector2.cxx
+++ src/visualdetector/src/detector2.cxx
@@ -4,6 +4,8 @@
 #include <vector>
 #include "CVInclude.h"
 #include <geometry_msgs/Vector3.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 
 cv::VideoCapture cap;
 
@@ -14,14 +16,15 @@ int main (int argc, char** argv)
 	ros::NodeHandle nh;
 	ros::Rate loop_rate (500);
 	sensor_msgs::ImagePtr msg;
-	image_transport::ImageTransport it (nh);
-	image_transport::Publisher pub = it.advertise ("detected_image", 1);
+	image_transport::ImageTransport it(nh);
+	image_transport::Publisher threshpub = it.advertise ("thresholded", 1);
+	image_transport::Publisher detectpub = it.advertise ("detected", 1);
 
 	// cv stuff
 	cv::Mat frame;
 	std::vector<std::vector<cv::Point> > contours;
 	std::vector<cv::Vec4i> hierarchy;
-	
+	int th, sub;
 	//camera choice
 	int camera = argv [1][0] - 48;
 
@@ -32,18 +35,8 @@ int main (int argc, char** argv)
 
 	cv::namedWindow ("thresholded");
 	cv::startWindowThread();
-	int th, rl, bl, gl, rh, bh, gh;
-	cv::createTrackbar ("Threshold", "thresholded", &th, 255);
-
-	cv::createTrackbar ("Red Low", "color", &rl, 255);
-	cv::createTrackbar ("Blue Low", "color", &bl, 255);
-	cv::createTrackbar ("Green Low", "color", &gl, 255);
-	cv::createTrackbar ("Red High", "color", &rh, 255);
-	cv::createTrackbar ("Blue High", "color", &bh, 255);
-	cv::createTrackbar ("Green High", "color", &gh, 255);
-
-	cv::Scalar colorLow (bl, gl, rl);
-	cv::Scalar colorHigh (bh, gh, rh);
+	cv::createTrackbar ("Threshold", "thresholded", &th, 20);
+	cv::createTrackbar ("Threshold", "thresholded", &sub, 200);
 
 	if (!cap.isOpened()) {
 		std::cout << "Unable to open camera "<< camera << std::endl;
@@ -57,17 +50,21 @@ int main (int argc, char** argv)
 		if (!frame.empty()) {
 
 			//find marker
-			cv::Mat resized, blurred, thresholded, gray, dilated;
+			cv::Mat thresh, gray, ero, dila, opening, closing;
 			cv::cvtColor (frame, gray, CV_BGR2GRAY);
-			cv::resize (gray, resized, cv::Size(), 0.50, 0.50, CV_INTER_LINEAR);
-			cv::GaussianBlur (resized, blurred,  cv::Size(11, 11), 0, 0);
-			cv::threshold (blurred, thresholded, th, 255, CV_THRESH_BINARY);
+			cv::adaptiveThreshold (gray, thresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 7, 0);
+			cv::erode (thresh, ero, 0, cv::Point(-1, -1), 2);
+			cv::dilate (ero, dila, 0, cv::Point(-1, -1), 2);
+			cv::morphologyEx (thresh, closing, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2), cv::Point(-1, -1)));
+			cv::morphologyEx (closing, opening, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2), cv::Point(-1, -1)));
 
 			// show stuff
 			cv::imshow ("original", frame);
-			cv::imshow ("resized", resized);
-			cv::imshow ("blurred", blurred);
-			cv::imshow ("thresholded", thresholded);
+			cv::imshow ("opened", opening);
+			cv::imshow ("dilated-erroded", ero);
+
+			threshpub.publish (cv_bridge::CvImage (std_msgs::Header(), "mono8", opening).toImageMsg());
+			detectpub.publish (cv_bridge::CvImage (std_msgs::Header(), "bgr8", frame).toImageMsg());
 			// stop if "q" is pressed
 			if (cv::waitKey(1) == 113)
 				break;
